93464c8e3a3770672d090829db185345a6d8cb8f
Vladlen Yamalov <v011088@yandex.ru>
2022-08-31 17:50:19 +0700

    add Predicat

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +11,10 @@ public class Main {
         list.add(new Person("Геннадий", "Иванов", 18));
         list.add(new Person("Аня", "Цветкова Петрова Сидорова", 17));
         list.add(new Person("Дмитрий", "Иванов Полянский", 20));
+        list.add(new Person("Юля", "Горшкова", 16));
+
+        Predicate<Person> personPredicate = person -> person.getAge() < 18;
+        list.removeIf(personPredicate);
 
         Collections.sort(list, new PersonSurnameLengthComparator());
         System.out.println(list);
